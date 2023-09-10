5e53a2c9f3a2ef1ec0797a216981046ada0bbaf8
DigitalPotential <Carlos@digitalpotential.se>
2023-09-10 11:59:26 +0200

    feat: Buttom premium variant, Dark Mode theme

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -21,7 +21,7 @@ export const Navbar = () => {
         </Link>
       </div>
       <div className="flex items-center gap-x-3">
-          <Button size="sm">
+          <Button size="sm" variant="premium">
             Upgrade
             <Sparkles className="h-4 w-4 fill-white text-white ml-2" />
           </Button>
