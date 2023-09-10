331467d7cdea5feb77035b9021fef6803294df71
DigitalPotential <Carlos@digitalpotential.se>
2023-09-11 00:00:10 +0200

    feat: Navbar, Sidebar, isPro Modal

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,7 @@
-import Navbar from "@/components/navbar";
+
+import { Navbar } from "@/components/navbar";
+import { Sidebar } from "@/components/sidebar";
+
 
 
 const RootLayout = async ({
@@ -6,10 +9,14 @@ const RootLayout = async ({
 }: {
   children: React.ReactNode;
 }) => {
+  const isPro = false; //todo check subscription status
 
   return ( 
     <div className="h-full">
-      <Navbar />
+      <Navbar isPro={isPro} />
+      <div className="hidden md:flex mt-16 w-20 flex-col fixed inset-y-0">
+        <Sidebar isPro={isPro} />
+      </div>
       <main className="md:pl-20 pt-16 h-full">
         {children}
       </main>
