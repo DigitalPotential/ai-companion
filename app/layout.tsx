270d666fad204eada9dfdb9a1bac96a65b10e000
DigitalPotential <Carlos@digitalpotential.se>
2023-09-10 23:42:49 +0200

    feat: Dark Mode config, Theme Toggle Button

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,9 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <ThemeProvider>{children}</ThemeProvider>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
