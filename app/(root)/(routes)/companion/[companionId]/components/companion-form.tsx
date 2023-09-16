53ab2c443477daa3ad85476ecfc65a80eda5e71f
DigitalPotential <Carlos@digitalpotential.se>
2023-09-17 00:13:37 +0200

    feat: CompanionForm WIP

--- app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
+++ app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -1,7 +1,9 @@
 import * as z from "zod";
 import { Category, Companion } from "@prisma/client";
-import { useForm } from "react-hook-form";
+import { Form, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Separator } from "@/components/ui/separator";
+import { FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
 
 interface CompanionFormProps {
   initialData: Companion | null;
@@ -45,11 +47,41 @@ export const CompanionForm = ({
     },
   });
 
-  const isLoading = form.formState.isSubmitting
+  const isLoading = form.formState.isSubmitting;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values)
-  }
+    console.log(values);
+  };
 
-  return <div>Companion form</div>;
+  return (
+    <div className="h-full p-4 space-y-2 max-w-3xl mx-auto">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 pb-10"
+        >
+          <div className="space-y-2 w-full col-span-2">
+            <div>
+              <h3 className="text-lg font-medium">General Information</h3>
+              <p className="text-sm text-muted-foreground">
+                General information about your Companion
+              </p>
+            </div>
+            <Separator className="bg-primary/10" />
+          </div>
+          <FormField
+            name="src"
+            render={({ field }) => (
+              <FormItem className="flex flex-col items-center justify-center space-y-4 col-span-2">
+                <FormControl>
+                    Image Upload Component
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </div>
+  );
 };
